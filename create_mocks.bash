7cc592c99be45ec6c04832e001f0773101c4db99
mehdirezaie <medirz90@icloud.com>
2022-01-26 16:10:39 -0500

    generate fnl cosmologies

--- create_mocks.bash
+++ create_mocks.bash
@@ -1,12 +1,24 @@
 
-ver=v0
+ver=v1
+tag=$1
 
 for iseed in {1..1000};
 do
-  MAPOUT=/home/mehdi/data/lognormal/${ver}/lrg-${iseed}-
-  CATOUT=/home/mehdi/data/lognormal/${ver}/lrg-cat-${iseed}.fits
+  clpref=/home/mehdi/github/flask/data/desiCl${tag}-
+  MAPOUT=/home/mehdi/data/lognormal/${ver}/lrg-${tag}-${iseed}-
+  #CATOUT=/home/mehdi/data/lognormal/${ver}/lrg-cat-${iseed}.fits
   
+  DIR="$(dirname "${MAPOUT}")"
+  echo $MAPOUT
+  echo "[${DIR}]"
+  du -h ${clpref}f1z1f1z1.dat
+  
+  if [ ! -d $DIR ]
+  then
+    echo $DIR "does not exist"
+    mkdir -p $DIR
+  fi
   #CATALOG_OUT: $CATOUT
-  bin/flask desi.config RNDSEED: $iseed MAPWERFITS_PREFIX: $MAPOUT
+  bin/flask desi.config CL_PREFIX: $clpref RNDSEED: $iseed MAPWERFITS_PREFIX: $MAPOUT
 done
 
