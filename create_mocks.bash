c62f93e6e0adaffc42fb72579a82bdfa64cdfee5
mehdirezaie <medirz90@icloud.com>
2022-03-11 10:19:36 -0500

    regenerate mocks with cats

--- create_mocks.bash
+++ create_mocks.bash
@@ -1,14 +1,14 @@
 # you need to set LD_LIBRARY_PATH appropriately, e.g.,
 # export LD_LIBRARY_PATH=/home/mehdi/local/lib64:${LD_LIBRARY_PATH}
 # where you have healpix_cxx, gsl, fitsio, and sharp library files
-ver=v1
+ver=v2
 tag=$1
 
 for iseed in {1..1000};
 do
   clpref=/home/mehdi/github/flask/data/desiCl${tag}-
-  MAPOUT=/home/mehdi/data/lognormal/${ver}/lrg-${tag}-${iseed}-
-  #CATOUT=/home/mehdi/data/lognormal/${ver}/lrg-cat-${iseed}.fits
+  MAPOUT=/DATA1/mehdi/lognormal/${ver}/lrghp-${tag}-${iseed}-
+  CATOUT=/DATA1/mehdi/lognormal/${ver}/lrgcat-${tag}-${iseed}.fits
   
   DIR="$(dirname "${MAPOUT}")"
   echo $MAPOUT
@@ -20,7 +20,6 @@ do
     echo $DIR "does not exist"
     mkdir -p $DIR
   fi
-  #CATALOG_OUT: $CATOUT
-  bin/flask desi.config CL_PREFIX: $clpref RNDSEED: $iseed MAPWERFITS_PREFIX: $MAPOUT
+  bin/flask desi.config CL_PREFIX: $clpref RNDSEED: $iseed MAPWERFITS_PREFIX: $MAPOUT CATALOG_OUT: $CATOUT
 done
 
